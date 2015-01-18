a77fea0662dd5795573deb0972fafa2b0c61a601
Masahiko Toyoshi <masahiko@Masahiko-no-MacBook-Air.local>
2015-01-18 17:32:50 +0900

    fix

--- .emacs.d/inits/40-python.el
+++ .emacs.d/inits/40-python.el
@@ -5,6 +5,9 @@
 (require 'auto-complete-config)
 (require 'python)
 
+(exec-path-from-shell-initialize)
+(exec-path-from-shell-copy-env "PATH")
+
 ;;;;; PYTHONPATH上のソースコードがauto-completeの補完対象になる ;;;;;
 (setenv "PYTHONPATH" "/usr/local/lib/python2.7/site-packages")
 (require 'jedi)
@@ -21,8 +24,8 @@
   (setq interpreter-mode-alist (cons '("python" . python-mode)
                                      interpreter-mode-alist)))
 
-;; flymake for python
-(add-hook 'python-mode-hook 'flymake-find-file-hook)
+;;; flymake for python
+(add-hook 'find-file-hook 'flymake-find-file-hook)
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -30,9 +33,25 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      ;; 昨日紹介した自作のシェルスクリプトを指定する
-      ;; 各々の環境にあわせて編集してください
-      (list "/usr/local/sbin/pyck"  (list local-file))))
+      (list "pycheckers"  (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pyflakes-init)))
 (load-library "flymake-cursor")
+(global-set-key [f10] 'flymake-goto-prev-error)
+(global-set-key [f11] 'flymake-goto-next-error)
+
+;; ;; flymake for python
+;; (add-hook 'python-mode-hook 'flymake-find-file-hook)
+;; (when (load "flymake" t)
+;;   (defun flymake-pyflakes-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name
+;;                         temp-file
+;;                         (file-name-directory buffer-file-name))))
+;;       ;; 昨日紹介した自作のシェルスクリプトを指定する
+;;       ;; 各々の環境にあわせて編集してください
+;;       (list "/usr/local/sbin/pyck"  (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks
+;;                '("\\.py\\'" flymake-pyflakes-init)))
+;; (load-library "flymake-cursor")
