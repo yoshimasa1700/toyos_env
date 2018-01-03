640029efdc4aa45eee6ead610d28839f6ffbf731
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2018-01-03 13:33:00 +0900

    Fix python setting.

--- .emacs.d/inits/20-package.el
+++ .emacs.d/inits/20-package.el
@@ -1,42 +1,29 @@
 (require 'package)
-;; MELPAのみ追加
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-;; パッケージ情報の更新
-;; 新しい環境にインストールする場合は以降のコメントを外すこと
-;; (package-refresh-contents)
-
-;; インストールするパッケージ
-;; (defvar my/favorite-packages
-;;   '(
-;;         ;;;; for auto-complete
-;;     auto-complete fuzzy popup pos-tip
-
-;; 		      ;;;; buffer utils
-;; 		 ;; popwin elscreen yascroll buffer-move
-
-;; 		      ;;;; flymake
-;; 		  flycheck flymake-jslint flymake-cursor
-
-;; 		      ;;;; go
-;; 		  ;;go-mode
-
-;; 		      ;;;; python
-;; 		  jedi
-;; 		      ;;;; helm
-;; 		  helm
-;; 		      ;;;; git
-;; 		  ;;magit git-gutter
-;; 		  ;; color theme
-;; 		  color-theme
-;; 		  tangotango-theme
-;; 		  exec-path-from-shell
-;; 		  irony
-;; 		  company-irony
-;; 		  ))
-
-;; my/favorite-packagesからインストールしていないパッケージをインストール
-;; (dolist (package my/favorite-packages)
-;;   (unless (package-installed-p package)
-;;     (package-install package)))
+(package-refresh-contents)
+(defvar my/favorite-packages
+  '(
+    auto-complete
+    fuzzy
+    popup
+    pos-tip
+    flycheck
+    flymake-jslint
+    flymake-cursor
+    ;; for python setting.
+    jedi
+    py-autopep8
+    flymake-python-pyflakes
+    ;; for global setting.
+    helm
+    color-theme
+    tangotango-theme
+    exec-path-from-shell
+    irony
+    company-irony
+    ))
+(dolist (package my/favorite-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
--- .emacs.d/inits/40-python.el
+++ .emacs.d/inits/40-python.el
@@ -1,57 +1,19 @@
-;===============
-; jedi (package.elの設定より下に書く)
-;===============
-(require 'epc)
-(require 'auto-complete-config)
-(require 'python)
-
-(exec-path-from-shell-initialize)
-(exec-path-from-shell-copy-env "PATH")
-
-;;;;; PYTHONPATH上のソースコードがauto-completeの補完対象になる ;;;;;
+;; setting for jedi.
 (setenv "PYTHONPATH" "/usr/local/lib/python2.7/site-packages")
+(setenv "PYTHONPATH" "/home/masahiko/.local/lib/python2.7/site-packages")
+
 (require 'jedi)
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
 
-;; python-mode をロードする
-(when (autoload 'python-mode "python-mode" "Python editing mode." t)
-  ;; python-mode のときのみ python-pep8 のキーバインドを有効にする
-  (setq python-mode-hook
-  (function (lambda ()
-    (local-set-key "\C-c\ p" 'python-pep8))))
-  (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-  (setq interpreter-mode-alist (cons '("python" . python-mode)
-                                     interpreter-mode-alist)))
+(setq ac-sources
+ (delete 'ac-source-words-in-same-mode-buffers ac-sources))
 
-;;; flymake for python
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list "pycheckers"  (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-(load-library "flymake-cursor")
-(global-set-key [f10] 'flymake-goto-prev-error)
-(global-set-key [f11] 'flymake-goto-next-error)
+;; setting for autopep8.
+(require 'py-autopep8)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
-;; ;; flymake for python
-;; (add-hook 'python-mode-hook 'flymake-find-file-hook)
-;; (when (load "flymake" t)
-;;   (defun flymake-pyflakes-init ()
-;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                        'flymake-create-temp-inplace))
-;;            (local-file (file-relative-name
-;;                         temp-file
-;;                         (file-name-directory buffer-file-name))))
-;;       ;; 昨日紹介した自作のシェルスクリプトを指定する
-;;       ;; 各々の環境にあわせて編集してください
-;;       (list "/usr/local/sbin/pyck"  (list local-file))))
-;;   (add-to-list 'flymake-allowed-file-name-masks
-;;                '("\\.py\\'" flymake-pyflakes-init)))
-;; (load-library "flymake-cursor")
+;; flymake.
+(add-hook 'python-mode-hook 'flymake-mode)
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
