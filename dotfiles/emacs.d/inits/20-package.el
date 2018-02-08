414933d9c1690a29639feb06b40c0372d412c035
Masahiko.Toyoshi <Masahiko.Toyoshi@sony.com>
2018-02-08 10:06:45 +0900

    Change.

--- dotfiles/emacs.d/inits/20-package.el
+++ dotfiles/emacs.d/inits/20-package.el
@@ -2,29 +2,29 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-(package-refresh-contents)
-(defvar my/favorite-packages
-  '(
-    auto-complete
-    fuzzy
-    popup
-    pos-tip
-    flycheck
-    flymake-jslint
-    flymake-cursor
-    ;; for python setting.
-    jedi
-    py-autopep8
-    flymake-python-pyflakes
-    ;; for global setting.
-    helm
-    color-theme
-    tangotango-theme
-    exec-path-from-shell
-    irony
-    company-irony
-    powerline
-    ))
-(dolist (package my/favorite-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; (package-refresh-contents)
+;; (defvar my/favorite-packages
+;;   '(
+;;     auto-complete
+;;     fuzzy
+;;     popup
+;;     pos-tip
+;;     flycheck
+;;     flymake-jslint
+;;     flymake-cursor
+;;     ;; for python setting.
+;;     jedi
+;;     py-autopep8
+;;     flymake-python-pyflakes
+;;     ;; for global setting.
+;;     helm
+;;     color-theme
+;;     tangotango-theme
+;;     exec-path-from-shell
+;;     irony
+;;     company-irony
+;;     powerline
+;;     ))
+;; (dolist (package my/favorite-packages)
+;;   (unless (package-installed-p package)
+;;     (package-install package)))
