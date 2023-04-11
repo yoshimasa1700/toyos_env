1de631d2bd09ec6f1764ad62f0ceca64c124b0a8
Masahiko Toyoshi <Masahiko.Toyoshi@sony.com>
2023-04-11 16:30:13 +0900

    Add RUST setting

---
+++ dotfiles/emacs.d/inits/42-rust.el
@@ -0,0 +1,28 @@
+(add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; #rust
+
+(use-package rust-mode
+  :ensure t
+  :custom rust-format-on-save t)
+
+
+(use-package cargo
+  :ensure t
+  :hook (rust-mode . cargo-minor-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; #lsp
+
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :init (yas-global-mode)
+;;   :hook (rust-mode . lsp)
+;;   :bind ("C-c h" . lsp-describe-thing-at-point)
+;;   :custom (lsp-rust-server 'rls))
+;; (use-package lsp-ui
+;;   :ensure t)
+
+(add-hook 'rust-mode-hook 'eglot-ensure)
+(add-to-list 'eglot-server-programs `(rust-mode . ("rust-analyzer")))
