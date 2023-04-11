8f967d9c6e85d3f7c25e81c30a44a83a98a3e7fc
Masahiko Toyoshi <Masahiko.Toyoshi@sony.com>
2023-04-11 16:28:25 +0900

    Add c default style

--- dotfiles/emacs.d/inits/41-cpp.el
+++ dotfiles/emacs.d/inits/41-cpp.el
@@ -1,14 +1,11 @@
 ;; for indent setting.
-(add-hook 'c-mode-hook
-          '(lambda () (setq tab-width 2)))
-(add-hook 'c++-mode-hook
-          '(lambda () (setq tab-width 2)))
-(defun my-c-c++-mode-init () (setq c-basic-offset 2) )
-(add-hook 'c-mode-hook
-          'my-c-c++-mode-init)
-(add-hook 'c++-mode-hook
-          'my-c-c++-mode-init)
 
+(defun my-c-c++-mode-init ()
+  (setq c-basic-offset 4)
+  )
+
+(add-hook 'c-mode-hook 'my-c-c++-mode-init)
+(add-hook 'c++-mode-hook 'my-c-c++-mode-init)
 
 ;; setting for trailing whitespace.
 (add-hook 'write-file-hooks 'delete-trailing-whitespace)
@@ -18,3 +15,7 @@
 
 (add-to-list 'eglot-server-programs `(c++-mode . ("clangd")))
 (add-to-list 'eglot-server-programs `(c-mode . ("clangd")))
+
+(setq c-default-style
+      '((c++-mode . "bsd")
+        (c-mode . "bsd")))
